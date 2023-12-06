1698b695812e721a7d5c30c3207c23e9f438c698
Shya <67426767+cungaminoan@users.noreply.github.com>
2023-12-06 23:56:49 +0700

    weapon manager

---
+++ Assets/Scripts/Weapon Scripts/WeaponManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField]
+    private WeaponHandler[] weapons;
+    private int currentWeaponIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentWeaponIndex = 0;
+        weapons[currentWeaponIndex].gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TurnOnSelectedWeapon(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            TurnOnSelectedWeapon(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            TurnOnSelectedWeapon(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            TurnOnSelectedWeapon(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            TurnOnSelectedWeapon(4);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            TurnOnSelectedWeapon(5);
+        }
+
+    }
+
+    protected void TurnOnSelectedWeapon(int weaponIndex)
+    {
+        if (currentWeaponIndex == weaponIndex)
+            return;
+        weapons[currentWeaponIndex].gameObject.SetActive(false);
+        weapons[weaponIndex].gameObject.SetActive(true);
+        currentWeaponIndex = weaponIndex;
+    }
+
+    public WeaponHandler GetWeaponHandler()
+    {
+        return weapons[currentWeaponIndex];
+    }
+}
